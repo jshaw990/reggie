1159d1472161913c43c879fc425288e1565bf0e3
Jayden Shaw <jayden.shaw@gmail.com>
2020-04-28 08:46:43 -0600

    JWT added to user registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const gravatar = require('gravatar'); 
 const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator');
+const config = require('config');
+const jwt = require('jsonwebtoken');
 
 const User = require('../../models/User');
 
@@ -56,7 +58,22 @@ router.post(
 
             await user.save();
 
-            res.send('User Registered');
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            };
+
+            jwt.sign(
+                payload, 
+                config.get('jwtSecret'), 
+                { expiresIn: 3600 },
+                (err, token) => {
+                    if(err) throw err;
+                    res.json({ token });
+
+                }
+            );
         } catch(err) {
             console.error(err.message);
             res.status(500).send('Server error');
